0dfed6a31b8cd74822e5360a7f47c1b885f13a6b
timothy-spencer <timothy.spencer@gsa.gov>
2018-03-05 17:40:47 -0800

    tried to turn off dev mode so it runs without tests

--- run_kibana.sh
+++ run_kibana.sh
@@ -1,13 +1,11 @@
-#!/bin/bash -x
-
-apt-get install -y jq
+#!/bin/bash
 
 ES_URI=$(echo "${VCAP_SERVICES}" | jq -r .elasticsearch56[0].credentials.uri)
 
 if grep ^elasticsearch.url config/kibana.yml >/dev/null ; then
 	echo elasticsearch.url is already configured
 else
-	echo "elasticsearch.url: \"${ES_URI}\"" >> config/kibana.dev.yml
+	echo "elasticsearch.url: \"${ES_URI}\"" >> config/kibana.yml
 fi
 
 # start the app up
