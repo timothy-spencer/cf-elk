f695842f71cf08dd07d41e674b2ec84079c1ff1e
timothy-spencer <timothy.spencer@gsa.gov>
2018-03-06 16:07:44 -0800

    switch to using release version instead of the dev version

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 # 
 # This script will try to configure/deploy Kibana into cloud.gov
 
+KIBANA_VERSION=5.6.8
+
 if cf services | grep OK >/dev/null ; then
 	echo cf seems to be up and going.
 	echo This deploy will be going into:
@@ -13,12 +15,23 @@ else
 fi
 
 if [ -d kibana ] ; then
-	echo kibana seems to be checked out already.  Leaving alone.
+	echo kibana seems to be pulled down already.  Leaving alone.
 else
-	git clone --branch v5.6.8 --single-branch https://github.com/elastic/kibana.git
+	if which shasum >/dev/null ; then
+		SHASUM="shasum"
+	else
+		SHASUM="sha1sum"
+	fi
+	wget -c https://artifacts.elastic.co/downloads/kibana/kibana-"${KIBANA_VERSION}"-linux-x86_64.tar.gz
+	if [ "$(${SHASUM} kibana-${KIBANA_VERSION}-linux-x86_64.tar.gz | awk '{print $1}')" != "4c08284c6b3c8225f8607fd349717cc3b37c1897" ] ; then
+		echo kibana package is corrupted, aborting
+		exit 2
+	fi
+	tar zxpf kibana-"${KIBANA_VERSION}"-linux-x86_64.tar.gz
+	mv kibana-"${KIBANA_VERSION}"-linux-x86_64 kibana
 fi
 
-cp run_kibana.sh package.json .node-version kibana/
+cp run_kibana.sh package.json kibana/
 
 if cf services | grep ^elk-elasticsearch >/dev/null ; then
 	echo elk-elasticsearch seems to be set up already, leaving alone
--- run_kibana.sh
+++ run_kibana.sh
@@ -9,4 +9,4 @@ else
 fi
 
 # start the app up
-npm start
+bin/kibana
